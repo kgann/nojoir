d5834681089e8d150d7f3c6b6887f554147aef3e
Kyle Gann <kylegann@gmail.com>
2012-08-04 01:11:48 -0400

    moved code to utils

--- src/nojoir/utils.clj
+++ src/nojoir/utils.clj
@@ -1,6 +1,15 @@
-(ns nojoir.utils)
+(ns nojoir.utils
+  (:use [clojure.data.json :only (json-str)]
+         clojure.data.xml))
 
 (defn xml-ready [col tbl]
   (map #(identity
           ['tbl {}
           (map vector (keys %) (vals %))]) col))
+
+(defn json [col tbl]
+  (json-str {tbl col}))
+
+(defn xml [col tbl]
+  (emit-str
+    (sexp-as-element [:root (xml-ready col tbl)])))
