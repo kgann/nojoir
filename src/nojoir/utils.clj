89df9d29a7a92bd9f04e947f26ac4d9c4f2ae3da
Kyle Gann <kylegann@gmail.com>
2012-08-04 12:20:41 -0400

    added content-type to response

--- src/nojoir/utils.clj
+++ src/nojoir/utils.clj
@@ -10,7 +10,7 @@
 ; .xml
 (defn xml-ready [col tbl]
   (map #(identity
-          [tbl {}
+          [(symbol tbl) {}
             (map vector (keys %) (vals %))])
   col))
 
--- src/nojoir/views/dal.clj
+++ src/nojoir/views/dal.clj
@@ -1,14 +1,15 @@
 (ns nojoir.views.dal
   (:require [nojoir.views.common :as common]
-            [nojoir.utils :as util])
-  (:use     [noir.core    :only [defpage]]
-            [hiccup.core  :only [html]]
+            [nojoir.utils        :as util])
+  (:use     [noir.core     :only [defpage]]
+            [noir.response :only [content-type]]
             korma.db korma.core))
 
 (defpage "/:conn/:db/:tbl.:format" {conn :conn db-name :db tbl :tbl f :format}
-  (let [conn-fn     (ns-resolve 'korma.db     (symbol conn))
-        response-fn (ns-resolve 'nojoir.utils (symbol f))]
+  (let [conn-fn      (ns-resolve 'korma.db      (symbol conn))
+        response-fn  (ns-resolve 'nojoir.utils  (symbol f))]
     (defdb db
       (conn-fn {:db db-name :user "root" :password "temp!@#$"}))
-    (response-fn
-      (select tbl) tbl)))
+    (content-type f
+      (response-fn
+        (select tbl) tbl))))
