92b473447a848e46617bbab1d0af8c580b01d408
Kyle Gann <kylegann@gmail.com>
2012-08-04 13:31:05 -0400

    field selection and readme

--- README.md
+++ README.md
@@ -12,11 +12,11 @@ lein run
 ```
 
 ## Querying
-#### returns all fields (select *) - for now!
 
 ```bash
   localhost:8080/:conn/:db/:table.:format
   localhost:8080/:conn/:db/:table.:format?w[:col]=:val&w[:b]=:val
+  localhost:8080/:conn/:db/:table.:format?w[:col]=:val&w[:b]=:val&&f=a,b
 ```
 
 Example
@@ -26,4 +26,7 @@ localhost:8080/mysql/some_database/some_table.json
 localhost:8080/oracle/some_database/some_table.xml
 localhost:8080/mysql/some_database/some_table.json?w[a]=1&w[b]=2
 # select * from some_database.some_table where a = 1 and b = 2
+localhost:8080/mysql/some_database/some_table.json?w[a]=1&w[b]=2&f=a,c
+# select a, c from some_database.some_table where a = 1 and b = 2
+
 ```
--- src/nojoir/views/dal.clj
+++ src/nojoir/views/dal.clj
@@ -5,13 +5,15 @@
             [noir.response :only [content-type]]
             korma.db korma.core))
 
-(defpage "/:conn/:db/:tbl.:format" {conn :conn db-name :db tbl :tbl f :format w :w}
+(defpage "/:conn/:db/:tbl.:format" {conn :conn db-name :db tbl :tbl fmt :format w :w f :f}
   (let [conn-fn      (ns-resolve 'korma.db      (symbol conn))
-        response-fn  (ns-resolve 'nojoir.utils  (symbol f))]
+        response-fn  (ns-resolve 'nojoir.utils  (symbol fmt))]
     (defdb db
       (conn-fn {:db db-name :user "root" :password "temp!@#$"}))
     (content-type f
-      (response-fn (if w
-                       (select tbl (where w))
-                       (select tbl))
+      (response-fn (select
+                     tbl (where (if w
+                                  w true))
+                         (fields (if f
+                                  f '*)))
         tbl))))
